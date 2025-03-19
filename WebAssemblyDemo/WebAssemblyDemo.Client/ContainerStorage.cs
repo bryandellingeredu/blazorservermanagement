c4dd19cdf916215b3ab553e46025ed71dd7ad493
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2025-03-19 09:44:10 -0400

    adding web assembly

---
+++ WebAssemblyDemo/WebAssemblyDemo.Client/ContainerStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Security;
+
+namespace WebAssemblyDemo.Client
+{
+    public class ContainerStorage
+    {
+        private string _message = string.Empty;
+
+        public string GetMesssage() { return _message; }   
+
+        public void SetMessage(string message) { _message = message; }
+    }
+}
